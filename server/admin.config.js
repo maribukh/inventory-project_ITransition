7a157dad30f6f6e7fbc0eaed5bf21bb4cdcf7901
Mariam <mariebukhaidze@gmail.com>
2025-11-04 01:06:08 +0400

    Added Firebabase Servive Account in admin.config.js

--- server/admin.config.js
+++ server/admin.config.js
@@ -1,11 +1,12 @@
 import admin from "firebase-admin";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
 
 export { admin };
